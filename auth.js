cfc9f079fcba1561938bfd09021c6fb17ca81185
D1p4Nj4Np4Nj4 <dipanjanpanja6@noreply.github.com>
2020-05-23 18:03:26 +0530

    k

--- auth.js
+++ auth.js
@@ -1,108 +1,193 @@
 const { admin } = require('./config/admin')
 const firebase = require('firebase')
-const randomId =require('random-id')
+const randomId = require('random-id')
 const cookieConfig = {
-    httpOnly: true, // to disable accessing cookie via client side js
-    secure: true, // to force https (if you use it)
-    maxAge: 1000000000, // ttl in ms (remove this option and cookie will die when browser is closed)
-    // signed: true // if you use the secret with cookieParser
-  };
+  httpOnly: true, // to disable accessing cookie via client side js
+  // secure: true, // to force https (if you use it)
+  maxAge: 1000000000, // ttl in ms (remove this option and cookie will die when browser is closed)
+  signed: true // if you use the secret with cookieParser
+};
 
-exports.signUp = async (req, res) => {
-    const email = req.body.email
-    const pass = req.body.pass
-    console.log(email, pass);
+exports.signUp = (req, res) => {
+  const email = req.body.email
+  const pass = req.body.pass
+  const name = req.body.name
+  console.log(email, pass);
 
-    if (email == "" || pass == "") return res.json({ error: true, message: "First input your credential" })
-    var user = await firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode == 'auth/weak-password') {
-            return res.json({ error: true, message: 'The password is too weak.' });
-        } else {
-            return res.json({ error: true, message: errorMessage });
-        }
-        console.log(error);
-        // ...
-    });
-    console.log(user);
-    return res.json({ token: user.user.getIdToken(), success: true })
+  if (email == "" || pass == "") return res.json({ error: true, message: "First input your credential" })
+  firebase.auth().createUserWithEmailAndPassword(email, pass).then(d => {
+    console.log(d.user.uid);
+    admin.firestore().doc(`/users/${d.user.uid}`).set({
+      name,
+      email,
+      createdAt: new Date().toISOString(),
+      uid: d.user.uid,
+      isActivated: false,
+      // verified: req.body.verified,
 
-}
-
-exports.login = async (req, res) => {
-    const email = req.body.email
-    const pass = req.body.pass
-    console.log(email, pass);
-
-    if (email == "" || pass == "") return res.json({ error: true, message: "First input your credential" })
-    const user = await firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode == 'auth/weak-password') {
-            return res.json({ error: true, message: 'The password is too weak.' });
-        } else {
-            return res.json({ error: true, message: errorMessage });
-        }
-    });
-    await firebase.auth().currentUser.getIdToken()
-    .then((idToken) => {
-        res.cookie('token', idToken, cookieConfig);
-        return res.json({ token: idToken, success: true })       
     })
-    .catch((error) => {
-        return res.json({ error: true, message: 'error' })
-    });
-    
+    firebase.auth().currentUser.getIdToken()
+      .then((idToken) => {
+        res.cookie('token', idToken, cookieConfig);
+        return res.json({ uid: d.user.uid,email:email,name:name, isActivated: false, success: true })
 
-}
-
-exports.gLogin = async(req, res) => {
-    const tokena = req.params.token;
-    var credential = firebase.auth.GoogleAuthProvider.credential(tokena);
-    let user_id;
-    let token = "";
-    let data = {};
-    firebase.auth().signInWithCredential(credential)
-      .then((d) => {
-        user_id = d.user.uid;
-        data = {
-          name: randomId(8, "A0"),
-          displayName: d.user.displayName,
-          email: d.user.email,
-          createdAt: new Date().toISOString(),
-          user_id: d.user.uid,
-          isAnonymous: d.user.isAnonymous,
-          userImage: d.user.photoURL,
-          // lastLogin: d.user.lastLoginAt,
-        };
-        // console.log(d.user.displayName);
-        return d.user.getIdToken();
-        
       })
-      .then((tokens) => {
-        token = tokens;
-        admin.firestore().collection("users").where("user_id", "==", user_id).limit(1).get()
-          .then((doc) => {
-            if (doc.docs[0]) {
-              return 0;
-            } else {
-              return admin.firestore().doc(`/users/${user_id}`).set(data);
-            }
-          });
-      })
-      .then(() => {
-        const device_key = randomId(24, "aA0");
-        res.cookie('token', token, cookieConfig);
-        return res
-          .status(200)
-          .json({ token, user_id, device_key, success: true });
-      })
-  
       .catch((error) => {
         console.log(error);
-        return res.status(403).json({ error: true, message: error.message });
+        return res.json({ error: true, message: error })
       });
+  }).catch(function (error) {
+    console.log(error);
+
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+      return res.json({ error: true, message: 'The password is too weak. Use minimum 6 digit' });
+    } else {
+      return res.json({ error: true, message: errorMessage });
+    }
+
+  });
+
+
+
 }
+
+exports.login = (req, res) => {
+  const email = req.body.email
+  const pass = req.body.pass
+  console.log(email, pass);
+
+  if (email == "" || pass == "") return res.json({ error: true, message: "First input your credential" })
+  firebase.auth().signInWithEmailAndPassword(email, pass).then(d => {
+
+    firebase.auth().currentUser.getIdToken()
+      .then((idToken) => {
+console.log(d.user.uid);
+
+        res.cookie('token', idToken, cookieConfig);
+    admin.firestore().collection('users').where('uid', '==', d.user.uid).limit(1).get().then(data => {
+      if (data.empty) {
+        admin.firestore().doc(`/users/${d.user.uid}`).set(email);
+        return res.json({ uid: d.user.uid,email:d.user.email, success: true, isActivated: false })
+      } else {
+        return res.json({ isActivated: data.docs[0].data().isActivated,name: data.docs[0].data().name,email: data.docs[0].data().email, uid: d.user.uid, success: true })
+      }
+    })
+
+      })
+      .catch((error) => {
+        console.log(error);
+        return res.json({ error: true, message: error })
+      });
+
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+      return res.json({ error: true, message: 'The password is too weak.' });
+    } else {
+      return res.json({ error: true, message: errorMessage });
+    }
+  });
+
+
+}
+
+exports.customLogin = async (req, res) => {
+  const data = {
+    isNewUser: req.body.isNewUser,
+    name: req.body.name,
+    email: req.body.email,
+    verified: req.body.verified,
+    userImage: req.body.userImage,
+    uid: req.body.uid,
+    isActivated: false,
+    // phoneNumber: req.body.phoneNumber,
+    createdAt: new Date().toISOString(),
+  }
+  if (data.isNewUser == true) {
+    admin.firestore().doc(`/users/${data.uid}`).set(data);
+
+    return res.json({ uid: data.uid, name:data.name,email:data.email, isActivated: false, success: true })
+
+    // admin.firestore().collection("users").where("user_id", "==", user_id).limit(1).get()
+    //   .then((doc) => {
+    //     if (doc.docs[0]) {
+    //       return 0;
+    //     } else {
+    //       return admin.firestore().doc(`/users/${user_id}`).set(data);
+    //     }
+    //   });
+  } else if (data.isNewUser == false) {
+    admin.firestore().collection('users').where('uid', '==', data.uid).limit(1).get().then(d => {
+      if (d.empty) {
+        console.log("data not found" + data.uid);
+
+        admin.firestore().doc(`/users/${data.uid}`).set(data);
+        return res.json({ uid: data.uid,name:data.name,email:data.email, success: true, isActivated: false })
+      } else {
+        return res.json({ isActivated: d.docs[0].data().isActivated,name:data.name,email:data.email, uid: data.uid, success: true })
+      }
+
+    })
+  }
+  console.log(data.isNewUser);
+
+
+};
+exports.activate = (req, res) => {
+  const data = {
+    name: req.body.name,
+    email: req.body.email,
+    date: req.body.date,
+    address: req.body.address,
+    Ph: req.body.Ph,
+    clg: req.body.clg,
+    isActivated: true,
+    // uid:req.body.uid
+
+  }
+  // console.log(data);
+  console.log(req.body.uid);
+  const myTestCookie = req.signedCookies.token;
+
+    console.log( myTestCookie);
+  try {
+    admin.firestore().doc(`/users/${req.body.uid}`).update(data);
+    return res.json({ success: true })
+  } catch (e) {
+    console.log(e);
+    return res.json({ error: true, message: e })
+  }
+}
+
+exports.checkLogin = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    return res
+      .status(403)
+      .json({ error: true, message: "Unauthorized", authorized: false });
+  }
+  admin.auth().verifyIdToken(idToken)
+    .then(function (decodedToken) {
+      req.user = decodedToken;
+      // console.log(req.user);
+      module.exports.uid = req.user.uid
+      module.exports.Verified = req.user.email_verified
+      res.cookie('token', idToken, cookieConfig);
+      return next()
+    }).catch(function (error) {
+      console.log('error while verifing token');
+      return res.status(403).json({ error: true, message: error });
+
+    });
+}
+
+
--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const useragent=require('express-useragent')
 const fileUpload=require('express-fileupload')
 const http=require('http')
 const bodyParser=require('body-parser')
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT||7000;
 
 var app = express()
-app.use(cross({origin:"https://techclub.web.app" ,credentials: true}));
+// app.use(cross({origin:"https://techclub.web.app" ,credentials: true}));
+app.use(cross({origin:"http://localhost:3000" ,credentials: true}));
 // app.use(cross())
-app.use(express.json());
+// app.use(express.json());
 // app.use(bodyParser())
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser("o vai maro mujhe maro"));
 
 
-const {login,gLogin,signUp} =require('./auth')
+const {login,checkLogin,signUp,customLogin,activate} =require('./auth')
 
 app.post('/signUp',signUp)
 app.post('/login',login)
-app.post('/login/:token',gLogin)
+app.post('/activate',activate)
+app.post('/customLogin',checkLogin,customLogin)
 
 app.listen(PORT);
